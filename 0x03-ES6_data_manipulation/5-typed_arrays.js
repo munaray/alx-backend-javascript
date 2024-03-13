1769e61a29b564212e29799ee6856d36be468db4
munaray <munahdesigns@gmail.com>
2024-03-13 03:58:09 +0100

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,7 @@
+/*
+comment
+*/
+
 export default function createInt8TypedArray(length, position, value) {
 	if (position >= length) throw new Error("Position outside range");
 	const res = new DataView(new ArrayBuffer(length), 0, length);
